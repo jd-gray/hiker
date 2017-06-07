0f37ec56df5c08b3b164570f1d102a0022cc41c3
Jared Gray <jared@jaredgray.us>
2017-06-07 16:19:48 -0700

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['aws_access_key_id'],                        # required
-      aws_secret_access_key: ENV['cTIzV0UCly+o1rp3PKfUURjfe/27yOg0s7jGFUpf'],                        # required
+      aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
   }
   config.fog_directory  = ENV['bucket']                          # required
 end
